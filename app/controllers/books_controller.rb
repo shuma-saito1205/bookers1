ed9fabcfd39481182f160fbe8a603372caf6b041
shumasaito <shushushu1205@gmail.com>
2025-03-26 04:15:38 +0000

    [add]ページ作成完了

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,11 +6,12 @@ class BooksController < ApplicationController
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to '/top'
+    redirect_to book_path(book.id)
   end
 
   def index
-    @books = Book.find(params[:id])
+    @book = Book.new
+    @books = Book.all
   end
 
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  get 'new' => 'books#new'
   get 'books' => 'books#index'
   get '/top' => 'homes#top'
   post 'books' => 'books#create'
-  get 'books/:id' => 'books#show'
-  get 'books/:id/edit' => 'books#edit', as: 'edit_list'
+  get 'books/:id' => 'books#show', as: 'book'
+  get 'books/:id/edit' => 'books#edit', as: 'edit'
+
+  get 'new' => 'books#new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :books
 end
