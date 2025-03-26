bd2773915617381f992cd9ff657e87fd267f4501
shumasaito <shushushu1205@gmail.com>
2025-03-26 08:11:15 +0000

    [add]ページ作成完了3

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,6 +7,7 @@ class BooksController < ApplicationController
     book = Book.new(book_params)
     book.save
     redirect_to book_path(book.id)
+    flash[:notice2] = "Book was successfully created."
   end
 
   def index
@@ -19,14 +20,23 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
 
   def destroy
     book = Book.find(params[:id])
     book.destroy
     redirect_to '/books'
+    flash[:notice1] = "Book was successfully destroyed."
   end
   
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+    flash[:notice2] = "Book was successfully updated."
+  end
+
   private
   def book_params
     params.require(:book).permit(:title, :body)
