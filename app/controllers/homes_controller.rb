0aec8f26d38062bd92d69f2095db1a271551bff7
shumasaito <shushushu1205@gmail.com>
2025-03-26 06:59:03 +0000

    [add]ページ作成完了2

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,4 +1,5 @@
 class HomesController < ApplicationController
   def top
+    render 'top'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
+  root 'homes#top'
   get 'books' => 'books#index'
-  get '/top' => 'homes#top'
   post 'books' => 'books#create'
   get 'books/:id' => 'books#show', as: 'book'
   get 'books/:id/edit' => 'books#edit', as: 'edit'
-
-  get 'new' => 'books#new'
+  delete 'books/:id' => 'books#destroy', as: 'destroy'
+  resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
